b28ceef2a6d4f6553286b18a1d1039bf9282a6f3
neok-m4700 <neok-m4700@users.noreply.github.com>
2016-07-30 18:57:27 +0200

    new structure

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from distutils.core import setup
 setup(name='fparser',
       version='1.0',
       description='Fortran parser',
-      py_modules=['api', 'Fortran2003', 'readfortran', 'parsefortran']
+      # py_modules=['api', 'Fortran2003', 'readfortran', 'parsefortran'],
+      packages = ['fparser']
       )
